0cdd9843c67aa1453661ab8c120247ff3490f39d
landim171 <daniel.landim@eaportal.org>
2025-06-04 22:27:06 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const botaoAbrir = document.querySelector(".header > button")
-const botaoFechar = document.querySelector(".header nav button")
+const botaoAbrir = document.querySelector("header > button")
+const botaoFechar = document.querySelector("header nav button")
 
-const body = document .querySelector("body")
-const nav = document .querySelector("header .mobile")
+const body = document.querySelector("body")
+const nav = document.querySelector("header .mobíle")
 
-botaoAbrir.addEventListener('click', abrirMenu)
+botaoAbrir.addEventListener('click', AbrirMenu)
 botaoFechar.addEventListener('click', fecharMenu)
 
-function abrirMenu () {
+function AbrirMenu() {
     body.classList.add("escurecer")
     nav.classList.add("abrir")
+}
+
+function fecharMenu() {
+    body.classList.remove("escurecer")
+    nav.classList.remove("abrir")
 }
